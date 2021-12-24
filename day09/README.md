028ad8e685fbf63e260178abb2abcc8ae254b57c
Nathan Patton <npatton@gmail.com>
2021-12-23 23:21:52 -0600

    Day 9, Part Two

--- day09/README.md
+++ day09/README.md
@@ -1,4 +1,4 @@
---- Day 9: Smoke Basin ---
+# --- Day 9: Smoke Basin ---
 These caves seem to be lava tubes. Parts are even still volcanically active; small hydrothermal vents release smoke into the caves that slowly settles like rain.
 
 If you can model how the smoke flows through the caves, you might be able to avoid it and be that much safer. The submarine generates a heightmap of the floor of the nearby caves for you (your puzzle input).
@@ -22,3 +22,54 @@ In the above example, there are four low points, all highlighted: two are in the
 The risk level of a low point is 1 plus its height. In the above example, the risk levels of the low points are 2, 1, 6, and 6. The sum of the risk levels of all low points in the heightmap is therefore 15.
 
 Find all of the low points on your heightmap. What is the sum of the risk levels of all low points on your heightmap?
+
+## --- Part Two ---
+Next, you need to find the largest basins so you know what areas are most important to avoid.
+
+A basin is all locations that eventually flow downward to a single low point. Therefore, every low point has a basin, although some basins are very small. Locations of height 9 do not count as being in any basin, and all other locations will always be part of exactly one basin.
+
+The size of a basin is the number of locations within the basin, including the low point. The example above has four basins.
+
+The top-left basin, size 3:
+
+```
+2199943210
+3987894921
+9856789892
+8767896789
+9899965678
+```
+
+The top-right basin, size 9:
+
+```
+2199943210
+3987894921
+9856789892
+8767896789
+9899965678
+```
+
+The middle basin, size 14:
+
+```
+2199943210
+3987894921
+9856789892
+8767896789
+9899965678
+```
+
+The bottom-right basin, size 9:
+
+```
+2199943210
+3987894921
+9856789892
+8767896789
+9899965678
+```
+
+Find the three largest basins and multiply their sizes together. In the above example, this is 9 * 14 * 9 = 1134.
+
+What do you get if you multiply together the sizes of the three largest basins?
--- day09/main.py
+++ day09/main.py
@@ -1,3 +1,6 @@
+from functools import reduce
+
+
 def read_input(filename: str) -> list[list[int]]:
     input_2d_array: list[list[int]] = []
 
@@ -17,33 +20,132 @@ def _clean_line(file_line: str) -> str:
     return file_line.replace("\n", "")
 
 
-if __name__ == "__main__":
-    heightmap: list[list[int]] = read_input("input.txt")
+def get_risk_level(point_array: list[list[int]]) -> int:
     total_risk_level: int = 0
 
-    for row_index, row in enumerate(heightmap):
+    for row_index, row in enumerate(point_array):
         for column_index, cell in enumerate(row):
             # Check above
             if row_index > 0:
-                if heightmap[row_index - 1][column_index] <= cell:
+                if point_array[row_index - 1][column_index] <= cell:
                     continue
 
             # Check below
-            if row_index < len(heightmap) - 1:
-                if heightmap[row_index + 1][column_index] <= cell:
+            if row_index < len(point_array) - 1:
+                if point_array[row_index + 1][column_index] <= cell:
                     continue
 
             # Check left
             if column_index > 0:
-                if heightmap[row_index][column_index - 1] <= cell:
+                if point_array[row_index][column_index - 1] <= cell:
                     continue
 
             # Check right
             if column_index < len(row) - 1:
-                if heightmap[row_index][column_index + 1] <= cell:
+                if point_array[row_index][column_index + 1] <= cell:
                     continue
 
             risk_level = cell + 1
             total_risk_level += risk_level
 
-    print(f"Total risk level = {total_risk_level}")
+    return total_risk_level
+
+
+def get_basins(point_array: list[list[int]]) -> list[set[str]]:
+    basin_list: list[set[str]] = []
+    processed_cells: set[str] = set()
+
+    for row_index, row in enumerate(point_array):
+        for column_index, cell in enumerate(row):
+            # If value is 9, not in a basin
+            if cell == 9:
+                continue
+
+            cell_id = f"{row_index}|{column_index}"
+
+            if cell_id in processed_cells:
+                continue
+
+            current_basin_set = get_basin(point_array, row_index, column_index, set())
+            processed_cells = processed_cells.union(current_basin_set)
+            basin_list.append(current_basin_set)
+
+    return basin_list
+
+
+def get_basin(
+    point_array: list[list[int]],
+    row_index: int,
+    column_index: int,
+    current_basin: set[str],
+) -> set[str]:
+    # Check above
+    if row_index > 0 and point_array[row_index - 1][column_index] < 9:
+        cell_id = f"{row_index - 1}|{column_index}"
+
+        if cell_id not in current_basin:
+            current_basin.add(cell_id)
+            current_basin = get_basin(
+                point_array, row_index - 1, column_index, current_basin
+            )
+
+    # Check below
+    if (
+        row_index < len(point_array) - 1
+        and point_array[row_index + 1][column_index] < 9
+    ):
+        cell_id = f"{row_index + 1}|{column_index}"
+
+        if cell_id not in current_basin:
+            current_basin.add(cell_id)
+            current_basin = get_basin(
+                point_array, row_index + 1, column_index, current_basin
+            )
+
+    # Check left
+    if column_index > 0 and point_array[row_index][column_index - 1] < 9:
+        cell_id = f"{row_index}|{column_index - 1}"
+
+        if cell_id not in current_basin:
+            current_basin.add(cell_id)
+            current_basin = get_basin(
+                point_array, row_index, column_index - 1, current_basin
+            )
+
+    # Check right
+    if (
+        column_index < len(point_array[0]) - 1
+        and point_array[row_index][column_index + 1] < 9
+    ):
+        cell_id = f"{row_index}|{column_index + 1}"
+
+        if cell_id not in current_basin:
+            current_basin.add(cell_id)
+            current_basin = get_basin(
+                point_array, row_index, column_index + 1, current_basin
+            )
+
+    return current_basin
+
+
+def get_product_size(basin_list: list[set[str]], number_of_basins: int) -> int:
+    basin_lengths: list[int] = [len(basin_set) for basin_set in basin_list]
+    basin_lengths.sort()
+
+    if len(basin_lengths) > number_of_basins:
+        basin_lengths = basin_lengths[-number_of_basins:]
+
+    return reduce((lambda x, y: x * y), basin_lengths)
+
+
+if __name__ == "__main__":
+    height_map: list[list[int]] = read_input("input.txt")
+
+    # Part One
+    print(f"Total risk level = {get_risk_level(height_map)}")
+
+    # Part Two
+    basins: list[set[str]] = get_basins(height_map)
+    product: int = get_product_size(basins, 3)
+
+    print(f"The product of the sizes of the three largest basins is {product}.")
