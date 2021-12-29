745374f8e6660c76724737113d73da475231109c
Nathan Patton <npatton@gmail.com>
2021-12-28 21:47:29 -0600

    Day 10

---
+++ day10/main.py
@@ -0,0 +1,123 @@
+from typing import Union
+
+OPEN_AND_CLOSE_CHARACTERS: dict[str, str] = {
+    "(": ")",
+    "[": "]",
+    "{": "}",
+    "<": ">",
+}
+
+ERROR_SCORES: dict[str, int] = {
+    ")": 3,
+    "]": 57,
+    "}": 1197,
+    ">": 25137,
+}
+
+COMPLETION_SCORES: dict[str, int] = {
+    ")": 1,
+    "]": 2,
+    "}": 3,
+    ">": 4,
+}
+
+
+def read_input(filename: str) -> list[str]:
+    input_lines: list[str] = []
+
+    with open(filename, "r", encoding="utf-8") as file_lines:
+        for file_line in file_lines:
+            input_lines.append(_clean_line(file_line))
+
+    return input_lines
+
+
+def _clean_line(file_line: str) -> str:
+    return file_line.replace("\n", "")
+
+
+def check_line(input_line: str) -> tuple[bool, list[str]]:
+    stack: list[str] = []
+
+    for character in input_line:
+        if len(stack) == 0:
+            if character not in OPEN_AND_CLOSE_CHARACTERS.keys():
+                return False, [character]
+
+        if character in OPEN_AND_CLOSE_CHARACTERS.keys():
+            stack.append(character)
+            continue
+
+        previous_character = stack.pop()
+
+        if character != OPEN_AND_CLOSE_CHARACTERS.get(previous_character):
+            return False, [character]
+
+    return True, stack
+
+
+def get_invalid_characters(input_lines: list[str]) -> list[str]:
+    invalid_characters: list[str] = []
+
+    for input_line in input_lines:
+        is_valid, invalid_character = check_line(input_line)
+
+        if not is_valid:
+            invalid_characters.extend(invalid_character)
+
+    return invalid_characters
+
+
+def get_syntax_error_score(input_lines: list[str]) -> int:
+    error_score: int = 0
+
+    for invalid_character in get_invalid_characters(input_lines):
+        error_score += ERROR_SCORES.get(invalid_character, 0)
+
+    return error_score
+
+
+def get_completion_string(stack: list[str]) -> str:
+    completion_string: str = ""
+
+    for character in reversed(stack):
+        completion_string += OPEN_AND_CLOSE_CHARACTERS.get(character, "")
+
+    return completion_string
+
+
+def get_completion_string_score(completion_string: str) -> int:
+    completion_string_score = 0
+
+    for character in completion_string:
+        completion_string_score *= 5
+        completion_string_score += COMPLETION_SCORES.get(character, 0)
+
+    return completion_string_score
+
+
+def get_final_completion_score(input_lines: list[str]) -> int:
+    completion_scores: list[int] = []
+
+    for input_line in input_lines:
+        is_valid, characters = check_line(input_line)
+
+        if not is_valid:
+            continue
+
+        completion_string = get_completion_string(characters)
+        completion_scores.append(get_completion_string_score(completion_string))
+
+    completion_scores.sort()
+
+    return completion_scores[len(completion_scores) // 2]
+
+
+if __name__ == "__main__":
+    lines = read_input("input.txt")
+
+    # Part One
+    print(f"The total syntax error score is {get_syntax_error_score(lines)}.")
+
+    # Part Two
+    print(f"The completion score is {get_final_completion_score(lines)}.")
---
+++ day10/test.py
@@ -0,0 +1,89 @@
+from day10.main import (
+    read_input,
+    check_line,
+    get_invalid_characters,
+    get_syntax_error_score,
+    get_completion_string,
+    get_completion_string_score,
+    get_final_completion_score,
+)
+
+
+def test_day_10_read_input():
+    sample_input = read_input("sample_input.txt")
+    assert sample_input == [
+        "[({(<(())[]>[[{[]{<()<>>",
+        "[(()[<>])]({[<{<<[]>>(",
+        "{([(<{}[<>[]}>{[]{[(<()>",
+        "(((({<>}<{<{<>}{[]{[]{}",
+        "[[<[([]))<([[{}[[()]]]",
+        "[{[{({}]{}}([{[{{{}}([]",
+        "{<[[]]>}<{[{[{[]{()[[[]",
+        "[<(<(<(<{}))><([]([]()",
+        "<{([([[(<>()){}]>(<<{{",
+        "<{([{{}}[<[[[<>{}]]]>[]]",
+    ]
+
+
+def test_day_10_part_one():
+    sample_input = read_input("sample_input.txt")
+
+    assert check_line(sample_input[0]) == (
+        True,
+        ["[", "(", "{", "(", "[", "[", "{", "{"],
+    )
+    assert check_line(sample_input[1]) == (True, ["(", "{", "[", "<", "{", "("])
+    assert check_line(sample_input[2]) == (False, ["}"])
+    assert check_line(sample_input[3]) == (
+        True,
+        ["(", "(", "(", "(", "<", "{", "<", "{", "{"],
+    )
+    assert check_line(sample_input[4]) == (False, [")"])
+    assert check_line(sample_input[5]) == (False, ["]"])
+    assert check_line(sample_input[6]) == (
+        True,
+        ["<", "{", "[", "{", "[", "{", "{", "[", "["],
+    )
+    assert check_line(sample_input[7]) == (False, [")"])
+    assert check_line(sample_input[8]) == (False, [">"])
+    assert check_line(sample_input[9]) == (True, ["<", "{", "(", "["])
+
+    assert get_invalid_characters(sample_input) == ["}", ")", "]", ")", ">"]
+
+    assert get_syntax_error_score(sample_input) == 26397
+
+
+def test_day_10_part_two():
+    sample_input = read_input("sample_input.txt")
+
+    _, stack0 = check_line(sample_input[0])
+    _, stack1 = check_line(sample_input[1])
+    _, stack3 = check_line(sample_input[3])
+    _, stack6 = check_line(sample_input[6])
+    _, stack9 = check_line(sample_input[9])
+
+    completion_string0 = get_completion_string(stack0)
+    completion_string1 = get_completion_string(stack1)
+    completion_string3 = get_completion_string(stack3)
+    completion_string6 = get_completion_string(stack6)
+    completion_string9 = get_completion_string(stack9)
+
+    assert completion_string0 == "}}]])})]"
+    assert completion_string1 == ")}>]})"
+    assert completion_string3 == "}}>}>))))"
+    assert completion_string6 == "]]}}]}]}>"
+    assert completion_string9 == "])}>"
+
+    score0 = get_completion_string_score(completion_string0)
+    score1 = get_completion_string_score(completion_string1)
+    score3 = get_completion_string_score(completion_string3)
+    score6 = get_completion_string_score(completion_string6)
+    score9 = get_completion_string_score(completion_string9)
+
+    assert score0 == 288957
+    assert score1 == 5566
+    assert score3 == 1480781
+    assert score6 == 995444
+    assert score9 == 294
+
+    assert get_final_completion_score(sample_input) == 288957
