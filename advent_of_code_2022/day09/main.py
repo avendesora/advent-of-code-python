46b08717bdc34e26d706e3f33100c1815c80c98a
Nathan Patton <npatton@gmail.com>
2022-12-09 00:34:13 -0600

    2022, Day 9

--- advent_of_code_2022/day09/main.py
+++ advent_of_code_2022/day09/main.py
@@ -1,16 +1,113 @@
+from __future__ import annotations
+
+from dataclasses import dataclass
+from enum import Enum
 from pathlib import Path
 
-
-def read_input(filename: Path | str) -> str | None:
-    return None
-
-
-def part_one(input_data: str | None) -> int | None:
-    return None
+from helpers import Point2D
+from helpers import read_input_as_string_array
 
 
-def part_two(input_data: str | None) -> int | None:
-    return None
+class Direction(Enum):
+    LEFT = "L"
+    RIGHT = "R"
+    UP = "U"
+    DOWN = "D"
+
+    @property
+    def x_value(self: Direction) -> int:
+        return DIRECTION_X_Y_VALUES[self][0]
+
+    @property
+    def y_value(self: Direction) -> int:
+        return DIRECTION_X_Y_VALUES[self][1]
+
+
+DIRECTION_X_Y_VALUES: dict[Direction, tuple[int, int]] = {
+    Direction.LEFT: (-1, 0),
+    Direction.RIGHT: (1, 0),
+    Direction.UP: (0, 1),
+    Direction.DOWN: (0, -1),
+}
+
+
+@dataclass
+class Motion:
+    direction: Direction
+    number_of_steps: int
+
+
+def read_input(filename: Path | str) -> list[Motion]:
+    lines = read_input_as_string_array(filename)
+    motions: list[Motion] = []
+
+    for line in lines:
+        direction, number_of_steps = line.split()
+        motions.append(Motion(Direction(direction), int(number_of_steps)))
+
+    return motions
+
+
+def move_next_knot(previous_knot: Point2D, current_knot: Point2D) -> Point2D:
+    x_diff = abs(current_knot.x - previous_knot.x)
+    y_diff = abs(current_knot.y - previous_knot.y)
+
+    # no need to move
+    if x_diff <= 1 and y_diff <= 1:
+        return current_knot
+
+    new_x = current_knot.x
+    new_y = current_knot.y
+
+    if x_diff > 0:
+        if previous_knot.x > current_knot.x:
+            new_x += 1
+        else:
+            new_x -= 1
+
+    if y_diff > 0:
+        if previous_knot.y > current_knot.y:
+            new_y += 1
+        else:
+            new_y -= 1
+
+    return Point2D(new_x, new_y)
+
+
+def get_tail_locations(
+    input_data: list[Motion], number_of_knots: int = 2
+) -> list[Point2D]:
+    starting_point = Point2D(0, 0)
+    knots: list[Point2D] = [starting_point for _ in range(number_of_knots)]
+    tail_locations: list[Point2D] = [starting_point]
+
+    for motion in input_data:
+        for _ in range(motion.number_of_steps):
+            for index, knot in enumerate(knots):
+                if index == 0:
+                    knot = Point2D(
+                        knot.x + motion.direction.x_value,
+                        knot.y + motion.direction.y_value,
+                    )
+                else:
+                    knot = move_next_knot(knots[index - 1], knot)
+
+                knots[index] = knot
+
+            if knots[-1] not in tail_locations:
+                tail_locations.append(knots[-1])
+
+    return tail_locations
+
+
+def part_one(input_data: list[Motion]) -> int:
+    tail_locations = get_tail_locations(input_data)
+    return len(tail_locations)
+
+
+def part_two(input_data: list[Motion]) -> int:
+    tail_locations = get_tail_locations(input_data, 10)
+    return len(tail_locations)
 
 
 if __name__ == "__main__":
--- advent_of_code_2022/day09/tests/day09_test.py
+++ advent_of_code_2022/day09/tests/day09_test.py
@@ -1,8 +1,8 @@
 import os
 from pathlib import Path
 
-import pytest
-
+from advent_of_code_2022.day09.main import Direction
+from advent_of_code_2022.day09.main import Motion
 from advent_of_code_2022.day09.main import part_one
 from advent_of_code_2022.day09.main import part_two
 from advent_of_code_2022.day09.main import read_input
@@ -10,19 +10,39 @@ from advent_of_code_2022.day09.main import read_input
 CURRENT_DIRECTORY = Path(os.path.dirname(os.path.realpath(__file__)))
 
 
-@pytest.mark.xfail()
 def test_read_input() -> None:
     input_data = read_input(CURRENT_DIRECTORY / "sample_input.txt")
-    assert input_data is not None
+    assert input_data == [
+        Motion(direction=Direction.RIGHT, number_of_steps=4),
+        Motion(direction=Direction.UP, number_of_steps=4),
+        Motion(direction=Direction.LEFT, number_of_steps=3),
+        Motion(direction=Direction.DOWN, number_of_steps=1),
+        Motion(direction=Direction.RIGHT, number_of_steps=4),
+        Motion(direction=Direction.DOWN, number_of_steps=1),
+        Motion(direction=Direction.LEFT, number_of_steps=5),
+        Motion(direction=Direction.RIGHT, number_of_steps=2),
+    ]
 
 
-@pytest.mark.xfail()
 def test_day_09_part_one() -> None:
     input_data = read_input(CURRENT_DIRECTORY / "sample_input.txt")
-    assert part_one(input_data) is not None
+    assert part_one(input_data) == 13
 
 
-@pytest.mark.xfail()
+def test_read_input2() -> None:
+    input_data = read_input(CURRENT_DIRECTORY / "sample_input2.txt")
+    assert input_data == [
+        Motion(direction=Direction.RIGHT, number_of_steps=5),
+        Motion(direction=Direction.UP, number_of_steps=8),
+        Motion(direction=Direction.LEFT, number_of_steps=8),
+        Motion(direction=Direction.DOWN, number_of_steps=3),
+        Motion(direction=Direction.RIGHT, number_of_steps=17),
+        Motion(direction=Direction.DOWN, number_of_steps=10),
+        Motion(direction=Direction.LEFT, number_of_steps=25),
+        Motion(direction=Direction.UP, number_of_steps=20),
+    ]
+
+
 def test_day_09_part_two() -> None:
-    input_data = read_input(CURRENT_DIRECTORY / "sample_input.txt")
-    assert part_two(input_data) is not None
+    input_data = read_input(CURRENT_DIRECTORY / "sample_input2.txt")
+    assert part_two(input_data) == 36
