a9fde447e9aabf9df722006ae7ad0dc7e89f0a50
Nathan Patton <npatton@gmail.com>
2022-12-08 21:08:22 -0600

    Day Nine, prep

---
+++ advent_of_code_2022/day09/main.py
@@ -0,0 +1,19 @@
+from pathlib import Path
+
+
+def read_input(filename: Path | str) -> str | None:
+    return None
+
+
+def part_one(input_data: str | None) -> int | None:
+    return None
+
+
+def part_two(input_data: str | None) -> int | None:
+    return None
+
+
+if __name__ == "__main__":
+    day9_input = read_input("input.txt")
+    print(part_one(day9_input))
+    print(part_two(day9_input))
